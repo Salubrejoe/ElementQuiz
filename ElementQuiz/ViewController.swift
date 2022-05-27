256bdf7cd6971c6bcfa7f7e3765259ea490b54f3
Licurgen <44895517+Salubrejoe@users.noreply.github.com>
2022-05-27 09:45:22 +0100

    The image of the first element should appear, and the answer label should show a question mark.

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateElement()
+        
     }
-
+    
+    // Shows image of the current Element
+    func updateElement() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        answerLabel.text = "?"
+    }
 
 }
 
